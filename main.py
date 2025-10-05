c9fed69ca00cce5538b3226209f8ba222baeeced
Ramadhan Zakki <ramadhanzakki506@gmail.com>
2025-10-05 13:49:10 +0700

    refactor: Modularize quiz logic into helper functions

--- main.py
+++ main.py
@@ -1,53 +1,19 @@
-quiz_success = 0
-number_question = 0
-
-
-def question(question, answer1, answer2, answer3, correct_answer):
-    global quiz_success
-    global number_question
-
-    number_question += 1
-
-    print(
-        f'Question {number_question}: {question}?\na) {answer1}\nb) {answer2}\nc) {answer3}')
-
-    while True:
-        user_answer = input('Your answer: ').lower()
-        if user_answer == 'a' or user_answer == 'b' or user_answer == 'c':
-            break
-        else:
-            print('\nWARNING: Please answer a, b, or c')
-            continue
-
-    answer = ''
-    if user_answer == 'a':
-        answer += answer1
-    elif user_answer == 'b':
-        answer += answer2
-    else:
-        answer += answer3
-
-    if answer == correct_answer:
-        print('Correct')
-        quiz_success += 1
-    else:
-        print('Wrong')
-        quiz_success += 0
+import question
 
 
 def geography():
-    question('What is Capital of France', 'Paris', 'London', 'Rome', 'Paris')
-    question('What is Largest Planet in Our Solar Sytem', 'Earth', 'Jupiter', 'Mars', 'Jupiter')
+    question.ask_question('What is Capital of France', 'Paris', 'London', 'Rome', 'Paris')
+    question.ask_question('What is Largest Planet in Our Solar Sytem', 'Earth', 'Jupiter', 'Mars', 'Jupiter')
 
 
 def history():
-    question('In what year did Indonesia proclaim its independence?', '1942', '1945', '1949', '1945')
-    question('The Battle of Surabaya, a symbol of Indonesian resistance, is commemorated annually on which date?', 'August 17', 'October 28', 'November 10', 'November 10')
+    question.ask_question('In what year did Indonesia proclaim its independence?', '1942', '1945', '1949', '1945')
+    question.ask_question('The Battle of Surabaya, a symbol of Indonesian resistance, is commemorated annually on which date?', 'August 17', 'October 28', 'November 10', 'November 10')
 
 
 def science():
-    question('What is the process where plants make their own food using sunlight?', 'Respiration', 'Photosynthesis', 'Transpiration', 'Photosynthesis')
-    question("What is the most abundant gas in the Earth's atmosphere?", 'Oxygen', 'Carbon Dioxide', 'Nitrogen', 'Nitrogen')
+    question.ask_question('What is the process where plants make their own food using sunlight?', 'Respiration', 'Photosynthesis', 'Transpiration', 'Photosynthesis')
+    question.ask_question("What is the most abundant gas in the Earth's atmosphere?", 'Oxygen', 'Carbon Dioxide', 'Nitrogen', 'Nitrogen')
 
 
 def main():
@@ -75,7 +41,7 @@ def main():
         else:
             print('Invalid Choice')
 
-    print(f'Your final score: {quiz_success}/{number_question}')
+    print(f'Your final score: {question.success() - 1}/{question.increment_question_count() - 1}')
 
 
 if __name__ == "__main__":
---
+++ question.py
@@ -0,0 +1,42 @@
+quiz_success = 0
+number_question = 0
+
+
+def success():
+    global quiz_success
+    quiz_success += 1
+    return quiz_success
+
+
+def increment_question_count():
+    global number_question 
+    number_question += 1
+    return number_question
+
+
+def ask_question(question, answer1, answer2, answer3, correct_answer):
+
+    print(
+        f'Question {increment_question_count()}: {question}?\na) {answer1}\nb) {answer2}\nc) {answer3}')
+
+    while True:
+        user_answer = input('Your answer: ').lower()
+        if user_answer == 'a' or user_answer == 'b' or user_answer == 'c':
+            break
+        else:
+            print('\nWARNING: Please answer a, b, or c')
+            continue
+
+    answer = ''
+    if user_answer == 'a':
+        answer += answer1
+    elif user_answer == 'b':
+        answer += answer2
+    else:
+        answer += answer3
+
+    if answer == correct_answer:
+        print('Correct')
+        success()
+    else:
+        print('Wrong')
